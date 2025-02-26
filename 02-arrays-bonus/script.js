900106ac5787b759d7438e834059ec825e0376d1
severogianluca <gianlucasevero300@gmail.com>
2025-02-26 23:18:34 +0100

    Feat: bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -19,9 +19,17 @@ const longNames = null;
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+// da ricordare con splice che il primo parametro indica 
+// l'indice da eliminare e il secondo quanti elementi se non lo 
+// specifico il secondo eliminera tutti gli elementi che prenderà il posto di quello eliminato
+teachers.splice(5, 1);
+console.log(teachers);
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString = teachers.join(', ');
+console.log(teachersString);
